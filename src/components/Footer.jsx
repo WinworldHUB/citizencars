10c99137e67598c957b462a991ec79e4c96479f5
Amit Dubey <iamsaagar@yahoo.com>
2023-03-25 15:24:45 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,35 +39,270 @@ const Footer = () => {
         </div>
       </div>
       <div class="content-area-fluid container-fluid">
-        <table className="table-borderless">
-          <tr>
-            <th>Luxury Cars</th>
-            <th>General</th>
-            <th>Style</th>
-            <th>Brands</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-          </tr>
-          <tbody>
-            <tr>
-              <td>
+        <div className="row">
+          <div className="col-2">
+            <h5 className="text-bold">Luxury cars</h5>
+            <ul>
+              <li>
                 <a href="javascript;">Why Us</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">About Us</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">FAQs</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">Help</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">Contact</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">Wallpaper</a>
-                <br />
+              </li>
+
+              <li>
                 <a href="javascript;">Our Showroom</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2">
+            <h5 className="text-bold">General</h5>
+            <ul>
+              <li>
+                <a href="javascript;">Sell Car</a>
+              </li>
+
+              <li>
+                <a href="javascript;">FAQs</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Videos</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Blog</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Auto Guide</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Car News</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Finance & Insurance</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Sales Login</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2">
+            <h5 className="text-bold">Style</h5>
+            <ul>
+              <li>
+                <a href="javascript;">Used SUV</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used Sedan</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used Convertible</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used Coupe</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used Sports</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used MUV-MPV</a>
+              </li>
+
+              <li>
+                <a href="javascript;">Used Hatchback</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2">
+            <h5 className="text-bold">Brands</h5>
+            <ul>
+              <li>
+                <a href="javascript;">Used Aston Martin</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Audi</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Avanturaa Choppers</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Bentley</a>
+              </li>
+              <li>
+                <a href="javascript;">Used BMW</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Buick</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Cadillac</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Chevrolet</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Chrysler</a>
+              </li>
+              <li>
+                <a href="javascript;">Used DC</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Ducati</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Ferrari</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Fiat</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2">
+            <h5 className="text-bold">&nbsp;</h5>
+            <ul>
+              <li>
+                <a href="javascript;">Used Ford</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Harley Davidson</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Honda</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Hummer</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Indian</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Jaguar</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Jeep</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Kawasaki</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Lamborghini</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Land Rover</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Lexus</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Maserati</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Maybach</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2">
+            <h5 className="text-bold">&nbsp;</h5>
+            <ul>
+              <li>
+                <a href="javascript;">Used McLaren</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Mercedes-Benz</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Mini</a>
+              </li>
+              <li>
+                <a href="javascript;">Used MV Agusta</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Nissan</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Porsche</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Rolls-Royce</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Skoda</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Toyota</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Triumph</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Volkswagen</a>
+              </li>
+              <li>
+                <a href="javascript;">Used Volvo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="content-area-fluid container-fluid bg-grey-3">
+        <div className="row">
+          <div className="col-3">
+            <h6 className="text-bold">Headquarters</h6>
+            <p>
+              Plot No. 134, Sector 37, Pace City 1, Gurgaon Haryana. 122001,
+              India
+            </p>
+            <p>sales@XYZ.com</p>
+          </div>
+          <div className="col-3">
+            <h6 className="text-bold">Studio</h6>
+            <p>
+              Plot No. 134, Sector 37, Pace City 1, Gurgaon Haryana. 122001,
+              India
+            </p>
+          </div>
+          <div className="col-3">
+            <h6 className="text-bold">Sell Car</h6>
+            <p>
+              Plot No. 134, Sector 37, Pace City 1, Gurgaon Haryana. 122001,
+              India
+            </p>
+          </div>
+          <div className="col-3">
+            <h6 className="text-bold">Vintage Car</h6>
+            <p>(+91) 9999 99999 89</p>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid bg-grey text-center p-3">
+        Copyright © 2023 Big XYZ
       </div>
     </>
   );
